d432439c05b2c4bf0c5eb158a123c8bc86b9768f
isabel cusick <cusick.isabel@gmail.com>
2016-10-20 14:54:20 -0500

    adds migrations for clients and products

---
+++ CNA_on_rails/db/migrate/20161020194809_create_clients.rb
@@ -0,0 +1,23 @@
+class CreateClients < ActiveRecord::Migration
+  def change
+    create_table :clients do |t|
+      t.string :organization
+      t.string :individual
+      t.boolean :legislation
+      t.string :legislation_notes
+      t.references :product, index: true, foreign_key: true
+      t.boolean :committees
+      t.string :commitees_notes
+      t.boolean :road
+      t.boolean :time
+      t.boolean :tech_savy
+      t.boolean :current_tracking_tool
+      t.boolean :others
+      t.string :others_notes
+      t.datetime :created_at
+      t.datetime :updated_at
+
+      t.timestamps null: false
+    end
+  end
+end
